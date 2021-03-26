6066cc48758ac127d2e9f030acde302b475d9fa1
srujan <dark00hood@gmail.com>
2021-03-26 13:53:53 +0530

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float movespeed = 600f;
+
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -movespeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+    public GameObject hexagonPrefab;
+
+    private float NextTimeToSpwan = 0f;
+    void Update()
+    {
+        if(Time.time >= NextTimeToSpwan)  
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+            NextTimeToSpwan = Time.time + 1f / spawnRate;
+        }
+    }
+}
